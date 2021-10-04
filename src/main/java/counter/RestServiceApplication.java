fee90186c7292353f92492a1e6bb46901e78a24c
Pontus <cpsoederlund@gmail.com>
2021-10-04 13:16:36 +0200

    try to make https work

--- src/main/java/counter/RestServiceApplication.java
+++ src/main/java/counter/RestServiceApplication.java
@@ -9,21 +9,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class RestServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RestServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-	return new WebMvcConfigurer() {
-        	@Override
-		public void addCorsMappings(CorsRegistry registry) {
-			registry.addMapping("/counter/**").allowedOrigins("https://clicker.psoder.net");
-			registry.addMapping("/counter/**").allowedOrigins("*");
-			registry.addMapping("/counter").allowedOrigins("*");
-			registry.addMapping("/counter/update").allowedOrigins("*");
-		}
-	};
-    }
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/counter/**").allowedOrigins("https://clicker.psoder.net");
+				registry.addMapping("/counter/**").allowedOrigins("*");
+				registry.addMapping("/counter/").allowedOrigins("*");
+				registry.addMapping("/counter/").allowedOrigins("https://10.179.121.88:9000");
+				registry.addMapping("/counter/").allowedOrigins("https://172.18.188.255:3000");
+				registry.addMapping("/counter/").allowedOrigins("https://localhost:3000");
+			}
+		};
+	}
 
 }
